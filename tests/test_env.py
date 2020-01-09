2587dd2cdd754c8ec23c75cb093e134660f24764
Cong Liu <cl3720@aqua.nyp.org>
2020-01-08 19:47:43 -0500

    test_env update

--- tests/test_env.py
+++ tests/test_env.py
@@ -4,18 +4,22 @@ import sys
 
 class TestEnv(unittest.TestCase):
     def test_ssh(self):
-        pwd = os.getcwd()
-        self.assertEqual(pwd, '/home/cl3720/2019-concept2vec/deep-learning-skeleton')
+        # pwd = os.getcwd()
+        # self.assertEqual(pwd, '/home/cl3720/2019-concept2vec/deep-learning-skeleton')
+        pass
 
     def test_py(self):
-        pyversion = sys.version
-        self.assertTrue(pyversion.startswith('3.5'))
+        self.assertTrue(sys.version.startswith('3.5'))
+
+    def test_tf(self):
+        import tensorflow as tf
+        self.assertTrue(tf.__version__ == '1.12.0')
+        self.assertTrue(tf.test.is_gpu_available())
     
     def test_keras_version(self):
-        pass
+        import keras as k
+        self.assertTrue(k.__version__ == '2.2.4')
 
-    def test_gpu_info(self):
-        pass
 
 if __name__ == "__main__":
     unittest.main()
