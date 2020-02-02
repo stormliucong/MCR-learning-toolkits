9e0e9235e6e3c26a40d843da1e0ab6c0fff398ee
Jay Lee <jayaos1002@gmail.com>
2020-02-02 03:15:46 -0500

    update glove

--- src/build_cooccur.py
+++ src/build_cooccur.py
@@ -1,24 +1,28 @@
 from scipy import sparse
+import itertools
+import numpy as np
+import sys
 
 
-def build_cooccur(vocab, corpus, window_size=10, min_count=None):
+def build_cooccur(vocab, corpus):
     """
     --vocab: dict form word2id
+    --corpus: list of patient visits
     """
     vocab_size = len(vocab)
-    id2word = dict((i, word) for (word, i) in word2id.items())
-
     cooccurrences = sparse.lil_matrix((vocab_size, vocab_size), dtype=np.float64)
 
-    for i, line in enumerate(corpus):
-        tokens = line.strip().split()
-        token_ids = [vocab[word][0] for word in tokens]
-        for pair in token_ids:
-            cooccurrences[pair[0], pair[1]] += 1
+    for visit in corpus:
+        visit_encode = [vocab[concept] for concept in visit]
+        permutations = itertools.permutations(visit_encode, 2)
+        for p in permutations:
+            cooccurrences[p[0], p[1]] += 1
 
     return cooccurrences
     
-
-
 if __name__=='__main__':
     vocab = sys.argv[1]
+    corpus = sys.argv[2]
+    build_cooccur(vocab, corpus)
+
+    # set the dir to save
--- src/glove.py
+++ src/glove.py
@@ -1,10 +1,18 @@
 import numpy as np
 
+class GloVe():
+     def __init__(self, embedding_dim, max_vocab_size=100000, min_occurrences=1, 
+     scaling_factor=3/4, cooccurr_ceil=100, batch_size=512, learning_rate=0.05):
+          self.embedding_dim = embedding_dim
+          self.max_vocab_size = max_vocab_size
+          self.min_occurrences = min_occurrences
+          self.scaling_factor = scaling_factor
+          self.cooccurr_ceil = cooccurr_ceil
+          self.batch_size = batch_size
+          self.learning_rate = learning_rate
+     
+     def build_comap(self, vocab, corpus):
+          pass
 
-
-def train_glove(vocab, cooccurrences, dim=128, iter=20, **kwargs):
-    vocab_size = len(vocab)
-
-    W = ((np.random.rand(vocab_size * 2, dim) - 0.5)
-         / float(dim + 1))
-    return pass
+     def build_model(self):
+          pass
