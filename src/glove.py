0a23156e0825004bb504693988456e3ccd9abcd4
Jay Lee <jayaos1002@gmail.com>
2020-02-05 23:05:27 -0500

    update glove trainer

--- src/glove.py
+++ src/glove.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from scipy import sparse
 import itertools
+import random
 import sys
 
 class GloVe(tf.keras.Model):
@@ -17,6 +18,7 @@ class GloVe(tf.keras.Model):
           self.learning_rate = learning_rate
           self.vocab_size = 0
           self.comap = None
+          self.epoch_loss_avg = []
      
      def build_comap(self, vocab, corpus):
           self.vocab_size = len(vocab)
@@ -40,40 +42,69 @@ class GloVe(tf.keras.Model):
           self.context_biases = tf.Variable(tf.random.uniform([self.vocab_size], 1.0, -1.0),
           name="context_biases")
 
-    def compute_cost(self, x):
-        """x = [target_ind, context_ind, co_occurrence_count]"""
-        target_emb = tf.nn.embedding_lookup([self.target_embeddings], x[0])
-        context_emb = tf.nn.embedding_lookup([self.context_embeddings], x[1])
-        target_bias = tf.nn.embedding_lookup([self.target_biases], x[0])
-        context_bias = tf.nn.embedding_lookup([self.context_biases], x[1])
+     def compute_cost(self, x):
+          """x = [target_ind, context_ind, co_occurrence_count]"""
+          target_emb = tf.nn.embedding_lookup([self.target_embeddicngs], x[0])
+          context_emb = tf.nn.embedding_lookup([self.context_embeddings], x[1])
+          target_bias = tf.nn.embedding_lookup([self.target_biases], x[0])
+          context_bias = tf.nn.embedding_lookup([self.context_biases], x[1])
 
-        weight = tf.math.minimum(1.0, 
-        tf.cast(tf.math.pow(
-             tf.math.truediv(x[2], self.max_vocab_size), 
-             self.scaling_factor),
-             dtype=tf.float32))
-        emb_product = tf.math.reduce_sum(tf.multiply(target_emb, context_emb), axis=0)
-        log_cooccurrence = tf.math.log(tf.cast(x[2], dtype=tf.float32))
+          weight = tf.math.minimum(1.0, 
+          tf.cast(tf.math.pow(
+               tf.math.truediv(x[2], self.max_vocab_size), 
+               self.scaling_factor),
+               dtype=tf.float32))
+          emb_product = tf.math.reduce_sum(tf.math.multiply(target_emb, context_emb), axis=1)
+          log_cooccurrence = tf.math.log(tf.cast(x[2], dtype=tf.float32))
 
-        distance_cost = tf.math.square(
+          distance_cost = tf.math.square(
                tf.math.add_n([emb_product, target_bias, context_bias, 
                tf.math.negative(log_cooccurrence)]))
+               
+          batch_cost = tf.math.reduce_sum(tf.multiply(weight, distance_cost))
           
-        cost = tf.multiply(weight, distance_cost)
-
-        return cost
-
+          return batch_cost
 
      def compute_gradients(self, x):
           with tf.GradientTape() as tape:
-               loss = model.compute_cost(x)
-     return loss, tape.gradient(loss, self.trainable_variables)
+               cost = self.compute_cost(x)
+          return cost, tape.gradient(cost, self.trainable_variables)
 
-     def get_batches(self):
+     def prepare_batch(self):
 
+          i_ids = []
+          j_ids = []
+          co_occurs = []
+
+          for i in range(self.comap.shape[0]):
+               for j in range(self.comap.shape[0]):
+                    if i == j: continue
+                    i_ids.append(i)
+                    j_ids.append(j)
+                    co_occurs.append(self.comap(i, j))
+     
+          assert len(i_ids) == len(j_ids), "The length of the data are not the same"
+          assert len(i_ids) == len(co_occurs), "The length of the data are not the same"
+          return i_ids, j_ids, co_occurs
 
      def train_GloVe(self, num_epochs, save_dir):
+          i_ids, j_ids, co_occurs = self.prepare_batch()
+          total_batch = int(np.ceil(len(i_ids) / self.batch_size))
+          cost_avg = tf.keras.metrics.Mean()
 
           for epoch in range(num_epochs):
-               pass
-     pass
+
+               for i in range(total_batch):
+                    i_batch = i_ids[i * self.batch_size : (i+1) * self.batch_size]
+                    j_batch = j_ids[i * self.batch_size : (i+1) * self.batch_size]
+                    co_occurs_batch = co_occurs[i * self.batch_size : (i+1) * self.batch_size]
+                    cost, gradients = self.compute_gradients([i_batch, j_batch, co_occurs_batch])
+                    self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+                    cost_avg(cost) 
+                    print("Step {}: Loss: {:.4f}".format(self.optimizer.iterations.numpy(), cost))
+
+          if (epoch % 1) == 0: 
+               avg_loss = cost_avg.result()
+               print("Epoch {}: Loss: {:.4f}".format(epoch, avg_loss))
+               self.epoch_loss_avg.append(avg_loss)
+               #save weights to the save_dir
