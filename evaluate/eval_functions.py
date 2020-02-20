11b86016b31a1d0381a505c7ba0898bfcdc1b1fa
Jay Lee <jayaos1002@gmail.com>
2020-02-20 13:11:01 -0500

    debug

--- evaluate/eval_functions.py
+++ evaluate/eval_functions.py
@@ -6,13 +6,3 @@ def cosine_sim(vec_a,vec_b):
     -- vec_a, vec_b : vectors to calculate cosine similarity between them
     """
     return (1 - cosine(vec_a,vec_b))
-
-def evaluate_structural_info():
-    pass
-
-def evaluate_cooccur_info():
-    pass
-
-def evaluate_phe_sim():
-    """use phe_pairs to evaluate representations"""
-    pass
